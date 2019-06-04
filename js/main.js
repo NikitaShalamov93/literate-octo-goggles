a18a0e97d7e778a0df8231fd929262e46efc981b
Nikita Shalamov <info@shar-landia.ru>
2019-06-04 23:01:37 +0300

    dldnbobks

--- js/main.js
+++ js/main.js
@@ -11,3 +11,25 @@ function trigerMobileMenu() {
 burger.addEventListener("click", function(){
     trigerMobileMenu();
 })
+
+/*Горизонтальный аккордион костыль */
+let teamAccojs = () => {
+    let teamList = document.querySelector(".team-acco");
+    
+    teamList.addEventListener('click', e => {
+        e.preventDefault();
+        let target = e.target;
+        const item = target.closest(".team-acco__item");
+        const items = document.querySelectorAll(".team-acco__item");
+        if(target.className === 'team-acco__trigger'){
+            if (!item.classList.contains("is-active")) {
+                for(let i=0; i<items.length;i++){
+                    items[i].classList.remove("active");
+                }
+                item.classList.add("active");
+            }
+        }
+    })
+                                    
+};
+teamAccojs();
