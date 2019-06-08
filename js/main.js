26c26c72225e7e88221c24f373c4e8aa4dcc10b6
Nikita Shalamov <info@shar-landia.ru>
2019-06-08 11:24:12 +0300

    hoolgool

--- js/main.js
+++ js/main.js
@@ -132,3 +132,51 @@ horizontalSlider(slideIndex);
 }; 
 
 downSlider();
+
+
+
+/* ПЛАВНАЯ ПРОКРУТКА */
+
+function animate(draw, duration) {
+
+let start = performance.now();
+
+requestAnimationFrame(function animate(time) {
+
+let timePassed = time - start;
+
+if (timePassed > duration) {
+
+timePassed = duration;
+
+}
+
+draw(timePassed);
+
+if (timePassed < duration) {
+
+requestAnimationFrame(animate);
+
+}
+
+});
+
+};
+
+let navigation = document.getElementsByTagName('nav')[0];
+
+navigation.addEventListener('click', function(event) {
+
+event.preventDefault();
+
+animate(function(timePassed) {
+
+let target = event.target,
+
+section = document.getElementById(target.getAttribute('href').slice(1));
+
+window.scrollBy(0, section.getBoundingClientRect().top / 20 - 3);
+
+}, 1200);
+
+});
