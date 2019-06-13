3685b657501a4e485b21e13785f4b0b513d25f46
Nikita Shalamov <info@shar-landia.ru>
2019-06-13 19:26:01 +0300

    gokalockabocka

--- js/main.js
+++ js/main.js
@@ -1,356 +1,262 @@
-const burger = document.querySelector(".burger");
-const menu = document.querySelector(".nav");
+window.addEventListener('DOMContentLoaded', () => {
 
-function trigerMobileMenu() {
-     console.log("function is work!");
-    burger.classList.toggle("active");
-    menu.classList.toggle("active");
-   document.body.classList.toggle("hidden");
-}
+    // Мобильное меню
+    const burger = document.querySelector(".burger");
+    const menu = document.querySelector(".nav");
 
-burger.addEventListener("click", function(){
-    trigerMobileMenu();
-})
-///метод меню от евгения
-function showMobileMenu() {
-   burger.classList.add("active");
-   menu.classList.add("active");
-   document.body.classList.add("hidden");
-}
+    function trigerMobileMenu() {
+        burger.classList.toggle("active");
+        menu.classList.toggle("active");
+        document.body.classList.toggle("hidden");
+    };
 
-function hideMobileMenu() {
-   burger.classList.remove("active");
-   menu.classList.remove("active");
-   document.body.classList.remove("hidden");
-}
- // И при клике на *burger* вызывать *showMobileMenu()*
-burger.addEventListener("click", function(){
-   showMobileMenu();
-})
-/*Горизонтальный аккордион костыль */
-let teamAccojs = () => {
-    let teamList = document.querySelector(".team-acco");
-    
-    teamList.addEventListener('click', e => {
-        e.preventDefault();
-        let target = e.target;
-        const item = target.closest(".team-acco__item");
-        const items = document.querySelectorAll(".team-acco__item");
-        if(target.className === 'team-acco__trigger'){
-            if (!item.classList.contains("active")) {
-                for(let i=0; i<items.length;i++){
-                    items[i].classList.remove("active");
+    burger.addEventListener("click", function() {
+        trigerMobileMenu();
+    });
+
+    ///метод меню от евгения
+    function showMobileMenu() {
+        burger.classList.add("active");
+        menu.classList.add("active");
+        document.body.classList.add("hidden");
+    };
+
+    function hideMobileMenu() {
+        burger.classList.remove("active");
+        menu.classList.remove("active");
+        document.body.classList.remove("hidden");
+    };
+
+    // Горизонтальный аккордион
+    let teamAccojs = () => {
+        let teamList = document.querySelector(".team-acco");
+        
+        teamList.addEventListener('click', e => {
+            e.preventDefault();
+            let target = e.target;
+            const item = target.closest(".team-acco__item");
+            const items = document.querySelectorAll(".team-acco__item");
+            if(target.className === 'team-acco__trigger'){
+                if (!item.classList.contains("active")) {
+                    for(let i=0; i<items.length;i++){
+                        items[i].classList.remove("active");
+                    }
+                    item.classList.add("active");
+                } else {
+                    item.classList.remove("active")
                 }
-                item.classList.add("active");
-            } else {
-                item.classList.remove("active")
             }
-        }
-    })
-};
-teamAccojs();
-
-/* ГОРИЗОНТАЛЬНЫЙ СЛАЙДЕР */ 
-
-function downSlider() { 
-
-let slideIndex = 1, 
-
-//Текущий слайд 
-
-slides = document.getElementsByClassName('slider__item'), 
-
-//Получаем псевдомассив слайдов 
-
-
-
-//Получаем контейнер слайдера 
-
-prev = document.querySelector('.slider__link_left'), 
-
-//Стрелка влево 
-
-next = document.querySelector('.slider__link_right'); 
-
-//Стрелка вправо 
-
-
-
-//Основная функция слайдера 
-
-function horizontalSlider(n) { 
-
-if (n > slides.length) { 
-
-//Возврат к первому слайду после перебора всего псевдомассива слайдов 
-
-slideIndex = 1; 
-
-}; 
-
-if (n < 1) { 
-
-//Возврат в конец псевдомассива после перебора всего псевдомассива слайдов в оратном направлении 
-
-slideIndex = slides.length; 
-
-}; 
-
-//Скрытие через цикл всех слайдов 
-
-for (let i = 0; i < slides.length; i++) { 
-slides[i].style.display = 'none'; 
-
-}; 
-
-//Установка видимости текущего слайда 
-slides[slideIndex - 1].style.display = 'flex'; 
-
-}; 
-
-//Функции увеличения индекса активного слайда. 
-
-//Получают данные (n) из обработчика событий prev и next 
-
-function plusSlides(n) { 
-
-horizontalSlider(slideIndex += n); 
-
-}; 
-
-function currentSlide(n) { 
-
-horizontalSlider(slideIndex = n); 
-
-}; 
-
-//Переключение на один слайд назад по клику 
-
-prev.addEventListener('click', function (e) { 
-e.preventDefault(); 
-plusSlides(1); 
-
-}); 
-
-//Переключение на один слайд вперед по клику 
-
-next.addEventListener('click', function (e) { 
-e.preventDefault(); 
-plusSlides(1); 
-
-}); 
-
-//Запуск слайдера 
-
-horizontalSlider(slideIndex); 
-
-}; 
-
-downSlider();
-
-
-
-/* ПЛАВНАЯ ПРОКРУТКА */
-
-function animate(draw, duration) {
-
-let start = performance.now();
-
-requestAnimationFrame(function animate(time) {
-
-let timePassed = time - start;
-
-if (timePassed > duration) {
-
-timePassed = duration;
-
-}
-
-draw(timePassed);
-
-if (timePassed < duration) {
-
-requestAnimationFrame(animate);
-
-}
-
-});
-
-};
-
-let navigation = document.getElementsByTagName('nav')[0];
-
-navigation.addEventListener('click', function(event) {
-trigerMobileMenu();
-event.preventDefault();
-
-animate(function(timePassed) {
-
-let target = event.target,
-
-section = document.getElementById(target.getAttribute('href').slice(1));
-
-window.scrollBy(0, section.getBoundingClientRect().top / 15 - 1);
-
-}, 1200);
-
-});
-
-
-
-
-// var hammer = new Hammer(document.documentElement);
-// hammer.get('swipe').set({
-//   direction: Hammer.DIRECTION_VERTICAL
-// });
-// hammer.on('swipeup', function () {
-//   if (currentEq >= sections.length - 1) {
-//     return;
-//   }
-
-//   if (!inScroll) {
-//     onePageScroll(currentEq += 1);
-//   }
-// });
-// hammer.on('swipedown', function () {
-//   if (currentEq <= 0) {
-//     return;
-//   }
-
-//   if (!inScroll) {
-//     onePageScroll(currentEq -= 1);
-//   }
-// });
-/////////////////////////////////////////////////////
-
-////////////////////////////////////////////////////
-// var hammer = new Hammer(document.documentElement);
-// hammer.get('swipe').set({
-//   direction: Hammer.DIRECTION_VERTICAL
-// });
-// hammer.on('swipeup', function () {
-//   if (currentEq >= sections.length - 1) {
-//     return;
-//   }
-
-//   if (!inScroll) {
-//     onePageScroll(currentEq += 1);
-//   }
-// });
-// hammer.on('swipedown', function () {
-//   if (currentEq <= 0) {
-//     return;
-//   }
-
-//   if (!inScroll) {
-//     onePageScroll(currentEq -= 1);
-//   }
-// }); 
-
-// mouseWheel
-
-// document.addEventListener('wheel', function (event) {
-//   function wheelDirection() {
-//     var delta = event.deltaY,
-//         direction = delta > 0 ? 'down' : 'up';
-//     return direction;
-//   }
-
-//   function scrollToSection() {
-//     var dir = wheelDirection();
-
-//     if (dir === 'down') {
-//       if (currentEq >= sections.length - 1) {
-//         return;
-//       }
-
-//       if (!inScroll) {
-//         onePageScroll(currentEq += 1);
-//       }
-//     } else {
-//       if (currentEq <= 0) {
-//         return;
-//       }
-
-//       if (!inScroll) {
-//         onePageScroll(currentEq -= 1);
-//       }
-//     }
-//   }
-
-//   scrollToSection();
-// }); 
-
-// Bullets
-
-
-
-
-var menuItems = document.querySelectorAll('.menu-acco__item'),
-    active = document.getElementsByClassName('active');
-Array.from(menuItems).forEach(function (item, i, menuItems) {
-  item.addEventListener('click', function (e) {
-    e.preventDefault();
-    if (active.length > 0 && active[0] !== this) // если есть активный элемент, и это не тот по которому кликнули
-      active[0].classList.remove('active'); // убрать класс active
-    
-      // изменить состояние класса active на текущем элементе: добавить если не было, убрать если было.
-    this.classList.toggle('active');
-  });
-});
-
-
-
-//////////////////////
-
-function createModal() {
-    const container = document.createElement('div');
-
-    container.className = 'popup';
-    container.innerHTML = template;
-
-    const contentTitle = container.querySelector('.overlay__title'),
-        contentBlock = container.querySelector('.overlay__text'),
-        closeBtns = container.querySelectorAll('.js-close'),
-        bigBtn = container.querySelector('.button'),
-        smallBtn = container.querySelector('.overlay__close');
-
-    for (var btn of closeBtns) {
-        btn.addEventListener('click', e => {
-            document.body.removeChild(container);
         })
-    }
+    };
+    teamAccojs();
 
-    container.addEventListener('click', e => {
-        if (e.target === container) {
-            btn.click();
-        }
-    })
+    // Гоизонтальный слайдер
+    function downSlider() { 
+        let slideIndex = 1, 
+            slides = document.getElementsByClassName('slider__item'), 
+            prev = document.querySelector('.slider__link_left'), 
+            next = document.querySelector('.slider__link_right'); 
 
-    return {
-        clear() {
-            contentTitle.innerHTML = '';
-            contentBlock.innerHTML = '';
-            smallBtn.style.display = "block";
-            bigBtn.style.display = "block";
-        },
-        open() {
-            document.body.appendChild(container);
-        },
-        close() {
-            closeBtn.click();
-        },
-        setTitle(title) {
-            contentTitle.innerHTML = title;
-        },
-        setContent(message) {
-            contentBlock.innerHTML = message;
-        },
-        setBigBtn() {
-            smallBtn.style.display = "none";
-        },
-        setSmallBtn() {
-            bigBtn.style.display = "none";
+        function horizontalSlider(n) { 
+
+            if (n > slides.length) { 
+                slideIndex = 1; 
+            }
+            
+            if (n < 1) { 
+                slideIndex = slides.length; 
+            }
+            
+            for (let i = 0; i < slides.length; i++) { 
+                slides[i].style.display = 'none'; 
+            }; 
+            
+            slides[slideIndex - 1].style.display = 'flex'; 
+        }; 
+
+        function plusSlides(n) { 
+            horizontalSlider(slideIndex += n); 
+        }; 
+
+        function currentSlide(n) { 
+            horizontalSlider(slideIndex = n); 
+        }; 
+
+        prev.addEventListener('click', function (e) { 
+            e.preventDefault(); 
+            plusSlides(1); 
+        }); 
+
+        next.addEventListener('click', function (e) { 
+            e.preventDefault(); 
+            plusSlides(1); 
+        }); 
+
+        horizontalSlider(slideIndex); 
+    }; 
+    downSlider();
+
+    // Плавная прокрутка
+    function animate(draw, duration) {
+        let start = performance.now();
+
+        requestAnimationFrame(function animate(time) {
+            let timePassed = time - start;
+
+            if (timePassed > duration) {
+                timePassed = duration;
+            }
+            draw(timePassed);
+
+            if (timePassed < duration) {
+                requestAnimationFrame(animate);
+            }
+        });
+
+    };
+
+    let navigation = document.getElementsByTagName('nav')[0];
+
+    navigation.addEventListener('click', function(event) {
+        trigerMobileMenu();
+        event.preventDefault();
+
+        animate(function(timePassed) {
+            let target = event.target,
+                section = document.getElementById(target.getAttribute('href').slice(1));
+                window.scrollBy(0, section.getBoundingClientRect().top / 15 - 1);
+
+        }, 1200);
+
+    });
+
+    // Меню (вертикальный аккордион)
+    let menuItems = document.querySelectorAll('.menu-acco__item'),
+        active = document.getElementsByClassName('active');
+    
+    Array.from(menuItems).forEach( item => {
+        item.addEventListener('click', function (e) {
+            e.preventDefault();
+            
+        if (active.length > 0 && active[0] !== this) 
+            active[0].classList.remove('active'); 
+            
+            this.classList.toggle('active');
+        });
+    });
+
+    // Попап отзывы
+    let reviewBtns = document.querySelectorAll('.review__view'),
+        close = document.querySelector('.popup-review__close'), // добавить крестик и дать ему этот класс
+        popupReview = document.querySelector('.popup-review');
+
+
+
+    function reviewShow() {
+        for(let i = 0; i < reviewBtns.length; i++) {
+            reviewBtns[i].addEventListener('click', (e) => {
+                e.preventDefault();
+                popupReview.style.display = 'flex';
+            });
         }
     };
-}
+    reviewShow();
+
+    document.onkeydown = (e) => {
+        if (e.keyCode === 27) {
+            popupReview.style.display = 'none';	        
+        }
+    };
+
+    window.addEventListener('click', function (e) {
+        let target = e.target;
+    
+        if (target.closest('.popup-review-content') && !target.closest(close)) {
+            e.stopPropagation();
+        } else if (target.closest('.popup-review')) popupReview.style.display = 'none';	
+    });
+
+    // close.addEventListener('click', (e) => {
+    //     e.preventDefault();
+    //     popupReview.style.display = 'none';
+    // });
+
+
+    // Отправка формы
+
+    // const myForm = document.querySelector('#orderForm'),
+//     template = document.querySelector('#modal-tmp').innerHTML,
+//     orderButton = document.querySelector('#order-button'),
+//     feedbacksCollection = document.querySelectorAll('.js-feedback-modal');
+
+// const popup = createModal();
+
+// for (var feedback of feedbacksCollection) {
+//     feedback.addEventListener('click', function (e) {
+//         var feedBackContent = this.previousElementSibling;
+//         var feedBackUser = feedBackContent.previousElementSibling;
+
+//         popup.clear();
+//         popup.open();
+//         popup.setContent(feedBackContent.innerHTML);
+//         popup.setTitle(feedBackUser.innerHTML);
+//         popup.setSmallBtn();
+//     });
+// }
+// orderButton.addEventListener('click', function (event) {
+//     event.preventDefault();
+
+//     let formValid = validateForm(myForm);
+
+//     if (formValid) {
+//         const data = {
+//             name: myForm.elements.name.value,
+//             phone: myForm.elements.phone.value,
+//             email: myForm.elements.email.value,
+//             comment: myForm.elements.comment.value
+//         };
+
+//         const xhr = new XMLHttpRequest();
+//         xhr.responseType = 'json';
+//         xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail');
+//         xhr.send(JSON.stringify(data));
+//         xhr.addEventListener('load', () => {
+//             console.log(xhr.response);
+//         });
+//         popup.clear();
+//         popup.open();
+//         popup.setContent('Сообщение отправлено');
+//         popup.setBigBtn();
+
+//     } else {
+//         console.log('smth wrong!');
+//         popup.clear();
+//         popup.open();
+//         popup.setContent('Сообщение не отправлено');
+//         popup.setBigBtn();
+//     };
+
+// });
+
+// function validateForm(myForm) {
+//     let valid = true;
+//     if (!myForm.checkValidity(myForm.elements.name)) {
+//         valid = false;
+//     }
+//     if (!myForm.checkValidity(myForm.elements.phone)) {
+//         valid = false;
+//     }
+//     if (!myForm.checkValidity(myForm.elements.email)) {
+//         valid = false;
+//     }
+//     return valid;
+// };
+});
+
+
+
+
+
+
+
+
